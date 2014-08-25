4a729a8c950f888125f0343ca124cacf90c69ac4
yulin1115 <719655143@qq.com>
2014-08-25 15:18:35 +0800

    sdf y2

--- ypblog/Public/Js/config.js
+++ ypblog/Public/Js/config.js
@@ -1,9 +1,9 @@
-function showmessage(message,title){
-		var content = '<div class="flickr" id="pageStr">'+message+'</div>';
-
-		var btns=[
-			{value:" 登 陆 ",onclick:"inserts("+id+")",focus:true},
-			{value:" 注 册 ",onclick:"popwin.close()"}
-		];
-		popwin.showDialog(3,title,content,btns,200,134);
+function showmessage(message,title){
+		var content = '<div class="flickr" id="pageStr">'+message+'</div>';
+
+		var btns=[
+			{value:" 登 陆 ",onclick:"inserts("+id+")",focus:true},
+			{value:" 注 册 ",onclick:"popwin.close()"}
+		];
+		popwin.showDialog(3,title,content,btns,200,134);
 }
